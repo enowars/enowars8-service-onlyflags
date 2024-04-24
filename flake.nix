fbaf0582027d208aaf64946b7f9bf4b717240f45
RTUnreal <unreal@rtinf.net>
2024-04-24 09:12:26 +0200

    add initial devshell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    systems,
+    nixpkgs,
+  }: let
+    # Small tool to iterate over each systems
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (system:
+        f (import nixpkgs {
+          inherit system;
+        }));
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.elixir_1_16
+        ];
+      };
+    });
+  };
+}
