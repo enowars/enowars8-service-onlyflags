1c6c230b03d2dfda4960e9452a45e92799e8954c
RTUnreal <unreal@rtinf.net>
2024-05-13 20:10:25 +0200

    add quick echo server for testing

---
+++ service/echo/src/main.rs
@@ -0,0 +1,20 @@
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:1337").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+             let (mut rd, mut wr) = socket.split();
+
+            if io::copy(&mut rd, &mut wr).await.is_err() {
+                eprintln!("failed to copy");
+            }
+        });
+    }
+}
+
