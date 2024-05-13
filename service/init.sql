b05dec30e9e295a315a922e3d703bcb61e975514
RTUnreal <unreal@rtinf.net>
2024-05-14 01:43:32 +0200

    add some more db definitions

--- service/init.sql
+++ service/init.sql
@@ -1,5 +1,14 @@
 CREATE DATABASE pod;
-CREATE USER "web"@"web";
+CREATE USER 'web';
+CREATE USER 'proxy';
 
-GRANT INSERT,SELECT ON pod.* TO "web"@"web";
+GRANT INSERT,SELECT ON pod.* TO "web";
+GRANT SELECT ON pod.* TO "proxy";
 
+use pod;
+CREATE TABLE user(
+  username VARCHAR(20) NOT NULL UNIQUE,
+  password VARCHAR(20) NOT NULL,
+  plan ENUM('premium', 'regular') NOT NULL,
+  created DATETIME NOT NULL
+);
