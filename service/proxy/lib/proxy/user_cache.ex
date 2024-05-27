526068700ea91337a2577a822050375ca24e6565
RTUnreal <unreal@rtinf.net>
2024-05-27 23:08:48 +0200

    fix(service): use correct service name

--- service/proxy/lib/proxy/user_cache.ex
+++ service/proxy/lib/proxy/user_cache.ex
@@ -68,7 +68,7 @@ defmodule Proxy.UserCache do
           Map.new()
         else
           premium = plan == "regular"
-          Map.put(prev.access, "premium_forum", premium)
+          Map.put(prev.access, "premium-forum", premium)
         end
     }
   end
