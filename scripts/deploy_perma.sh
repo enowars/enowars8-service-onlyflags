ea9b26cc6c637ea3e7d8ef7f8972a686f96e9f68
RTUnreal <unreal@rtinf.net>
2024-07-04 23:55:17 +0200

    scripts: add build step to deploy_perma script

--- scripts/deploy_perma.sh
+++ scripts/deploy_perma.sh
@@ -7,5 +7,8 @@ ROOT="$(dirname "$(dirname "$0")")"
 rsync -a --exclude=data "$ROOT/checker" checker:/services/checker_onlyflags
 rsync -a --exclude=data "$ROOT/service" onlyflags:/services/onlyflags
 
+ssh checker /usr/bin/env bash -c '"cd /services/checker_onlyflags/checker && docker compose build"'
+ssh onlyflags /usr/bin/env bash -c '"cd /services/onlyflags/service && docker compose build"'
+
 ssh checker /usr/bin/env bash -c '"cd /services/checker_onlyflags/checker && docker compose down && docker compose up -d"'
 ssh onlyflags /usr/bin/env bash -c '"cd /services/onlyflags/service && docker compose down && docker compose up -d"'
